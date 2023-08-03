7ff17dbc0495849a0f7171096f04472c8c3df567
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-03 16:47:00 +0700

    Initial commit

---
+++ TodoListUbersnap/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TodoListUbersnap
+//
+//  Created by Yuga Samuel on 03/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoListUbersnap/TodoListUbersnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListUbersnapApp.swift
+//  TodoListUbersnap
+//
+//  Created by Yuga Samuel on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListUbersnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
